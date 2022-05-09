29c21370746d4dffbac66e541747ca5001a198dd
Saravanankoki <sudar171296@gmail.com>
2022-05-09 20:05:41 +0530

    Pojo Concepts

---
+++ src/com/java/Details.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class Details extends Student{
+	
+	public static void main(String[] args) {
+		
+		studentName("saravanan");
+        studentRollNo(123);
+        
+        studentName("senthil");
+        studentRollNo(525);
+		
+	}
+	
+	
+
+}
---
+++ src/com/java/Student.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Student {
+	
+	public static void studentName(String name) {
+      System.out.println("Student name :" +name);
+	}
+	public static void studentRollNo(int rollno) {
+		System.out.println("Student Roll no :" +rollno);
+	}
+
+}
